d056cbabefe1564238db085722195e9f26045ee0
Diego GTG <diegogomeztg@outlook.com>
2026-05-20 15:58:55 -0600

    Configuracion de base de datos

---
+++ src/main/java/com/risosuit/CajeroService/Configuration/DataSourceConfig.java
@@ -0,0 +1,29 @@
+package com.risosuit.CajeroService.Configuration;
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+    
+@Configuration
+public class DataSourceConfig {
+
+
+    @Bean
+    public DataSource dataSource(){
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("oracle.jdbc.OracleDriver");
+        dataSource.setUrl("jdbc:oracle:thin:@localhost:1521:orcl");
+        dataSource.setUsername("DGomezTagleGCajero");
+        dataSource.setPassword("password1");
+        return dataSource;
+    }
+
+    @Bean
+    public JdbcTemplate jdbcTemplate (DataSource dataSource){
+        JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+        return jdbcTemplate;
+    } 
+}
